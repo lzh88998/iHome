4f0437c6b85e2a796a123e2983d93de632ca71db
lzh88998 <frog88998@163.com>
2022-05-05 22:24:08 -0700

    add comments for each function

--- src/log.c
+++ src/log.c
@@ -3,6 +3,11 @@
 #include "log.h"
 
 static int gs_log_level = LOG_LEVEL_ERROR;
+
+/*
+ * Parse the input string and convert it to an internal
+ * numberical value and store it as current log level
+ */
 int log_set_level(const char* level) {
     if(0 == strcmp(LOG_LEVEL_ERROR_NAME, level))
 	gs_log_level = LOG_LEVEL_ERROR;
@@ -23,6 +28,26 @@ int log_set_level(const char* level) {
     return 0;
 }
 
+/*
+ * This function will print the valid log info levels
+ * and corresponding string values for reference.
+ * Usually used in response of invalid input to 
+ * log_set_level.
+ */
+void log_print_level_info(void) {
+    printf("Valid log leves are: \r\n");
+    printf("%s\tOnly show error messages. \r\n", LOG_LEVEL_ERROR_NAME);
+    printf("%s\tShow warning and error messages. \r\n", LOG_LEVEL_ERROR_WARNING);
+    printf("%s\tShow information, warning and error. \r\n", LOG_LEVEL_ERROR_INFO);
+    printf("%s\tShow debug, information, warning and error. \r\n", LOG_LEVEL_ERROR_DEBUG);
+    printf("%s\tShow all messages. \r\n", LOG_LEVEL_ERROR_DETAILS);
+}
+
+/*
+ * This is used in below macros to print messages.
+ * User are not encouraged to use this directly, but
+ * it can be called from end user.
+ */
 void log_with_level(const int level, const char* fmt, ...) {
     va_list ap;
     va_start(ap, fmt);
@@ -32,13 +57,4 @@ void log_with_level(const int level, const char* fmt, ...) {
 	printf("\r\n");
     }
     va_end(ap);
-}
-
-void log_print_level_info(void) {
-    printf("Valid log leves are: \r\n");
-    printf("%s\tOnly show error messages. \r\n", LOG_LEVEL_ERROR_NAME);
-    printf("%s\tShow warning and error messages. \r\n", LOG_LEVEL_ERROR_WARNING);
-    printf("%s\tShow information, warning and error. \r\n", LOG_LEVEL_ERROR_INFO);
-    printf("%s\tShow debug, information, warning and error. \r\n", LOG_LEVEL_ERROR_DEBUG);
-    printf("%s\tShow all messages. \r\n", LOG_LEVEL_ERROR_DETAILS);
 }
